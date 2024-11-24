911c5caaf871fb48146039e83f5bc2424bf93fec
Paszczak1987 <lpaszko30@gmail.com>
2024-11-24 23:16:07 +0100

    staff_hub app works and bootstrap plugged

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from staff_hub import views as staff_hub
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', staff_hub.BaseView.as_view(), name='home'),
 ]
